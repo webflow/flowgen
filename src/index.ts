7c65f2fbbd027dc9ea12e01c4ba30f4fa52fbb6d
Dan Dean <me@dandean.com>
2023-07-14 14:48:29 -0700

    Use code snipped instead of snapshot to assert result

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import compiler from "./cli/compiler";
-import beautify from "./cli/beautifier";
+import * as beautify from "./cli/beautifier";
 
 export { default as compiler } from "./cli/compiler";
 
 export { default as beautify } from "./cli/beautifier";
 
+export const tsBeautify = beautify.tsBeautify;
+export const flBeautify = beautify.flBeautify;
+
 export default {
-  beautify,
+  beautify: beautify.default,
   compiler,
 };
